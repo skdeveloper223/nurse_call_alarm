364ff2121f53e68c3726079829f5f24b9ae7fa0c
santosh <kolisantosh222@gmail.com>
2024-03-07 18:49:18 +0530

    auth controller

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  Rx<User?> firebaseUser = Rx<User?>(null);
+
+  String? email, password, userType;
+
+  @override
+  void onInit() {
+    super.onInit();
+    firebaseUser.bindStream(_auth.authStateChanges());
+  }
+
+  void signIn() async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email!, password: password!);
+    } catch (e) {
+      Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+
+  void register() async {
+    try {
+      await _auth.createUserWithEmailAndPassword(email: email!, password: password!);
+      await _auth.currentUser!.updateDisplayName(userType!);
+    } catch (e) {
+      Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
+    }
+  }
+
+  void signOut() async {
+    await _auth.signOut();
+  }
+}
